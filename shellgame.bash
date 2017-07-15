93202963b248fc46b2c55c8713c5d709aec005d7
Kiren Srinivasan <srinitude@gmail.com>
2017-07-15 22:22:30 +0000

    Makes some bash syntax fixes

--- shellgame.bash
+++ shellgame.bash
@@ -35,7 +35,7 @@ function diff_error()
 function print_diff()
 {
     if [ $1 == $OUTPUT_DIFF ]; then
-	if [ -s $1 ]
+	if [ -s $1 ]; then
 	    print_fail
 	    echo "The output between your shell and 'sh' differ"
 	    cat "$DIFF"
@@ -43,10 +43,12 @@ function print_diff()
 	    print_pass
 	fi
     elif [ $1 == $ERROR_DIFF ]; then
-	if [ -s $1 ]
+	if [ -s $1 ]; then
 	    print_fail
 	    echo "The errors between your shell and 'sh' differ"
 	    cat "$DIFF"
+	else
+	    print_pass
     else
 	echo -n "What are you diffing dude?\n"
     fi
