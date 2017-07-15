f943a5c9bb35490650d8eda90e92d9d765d75caf
Kiren Srinivasan <srinitude@gmail.com>
2017-07-15 20:12:11 +0000

    Adds color printing for pass/fail

--- shellgame.bash
+++ shellgame.bash
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 ###################################
+function print_fail()
+{
+    echo -en ""$RED"FAIL"$RESET"\n"
+}
+###################################
+function print_pass()
+{
+    echo -en ""$GREEN"PASS"$RESET"\n"
+}
+###################################
+function print_error()
+{
 
-
+}
 
 # Load the configuration file
 source config
+
